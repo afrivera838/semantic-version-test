b6a52d724de1fd306bbf5b80f961cd36da17fb64
Andres Rivera <arivera@abatechnology.com>
2023-08-11 14:39:59 -0500

    success

--- src/modules/example/Example.tsx
+++ src/modules/example/Example.tsx
@@ -3,7 +3,6 @@ import stl from './styles/index.module.scss';
 
 const Example = () => (
   <div>
-    <Box></Box>
     <h1 className={`${stl.colorBlue} ${stl.smallText}`}>Hello Boilerplate!</h1>
   </div>
 );
